ff92e799a1690f2e913d311bf8aa58741f8900a2
CaMoCBaJL <55454125+CaMoCBaJL@users.noreply.github.com>
2021-08-01 21:24:08 +0400

    Added entity ChainElementSettings to simplify the serialization of separate chain elements work results.

---
+++ automats/AutomatFunctions/ChainElementSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AutomatFunctions
+{
+    class ChainElementSettings
+    {
+        [JsonProperty]
+        public string AutomatDataFile { get; private set; }
+        
+        [JsonProperty]
+        public string InputSignalString { get; private set; }
+
+        [JsonProperty]
+        public string OutputSignalString { get; private set; }
+
+
+        protected ChainElementSettings() { }
+
+
+    }
+}
