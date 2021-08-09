11bef3686ff61511fa86494466d7e95b15d245ad
CaMoCBaJL <55454125+CaMoCBaJL@users.noreply.github.com>
2021-08-09 20:46:04 +0400

    Started my implementation of binary tree

---
+++ automats/BuisnessLogic/CryptoStrengthTestLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLInterfaces;
+using Entities;
+
+namespace BuisnessLogic
+{
+    class CryptoStrengthTestLogic : ICryptoStrengthTestLogic
+    {
+        public StrengthTestResultMarks MarkTestResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ParseInputData(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ModellingStepData ParseModellingStepResult(List<AutomatConfiguration> data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ automats/CollectionLibrary/BinaryTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace CollectionLibrary
+{
+    public class BinaryTree
+    {
+        List<ModellingStepData> _storage;
+
+
+        public BinaryTree()
+        {
+            _storage = new List<ModellingStepData>();
+        }
+
+        public BinaryTree (IEnumerable<ModellingStepData> data)
+        {
+            _storage = data.ToList();
+        }
+
+        public bool Add(ModellingStepData stepData)
+        {
+            return true;
+        }
+
+        public bool Remove(int index)
+        {
+            return true;
+        }
+
+        int BinarySearch(int elementOutputSignals)
+        {
+            int n = _storage.Count;
+
+            int step = n / 2;
+
+            n -= step;
+
+            while (step != 1)
+            {
+                step = step / 2 + step % 2;
+
+                if (_storage[n].OutputSignals == elementOutputSignals)
+                    return n;
+
+                if (_storage[n].OutputSignals > elementOutputSignals)
+                    n -= step;
+                else
+                    n += step;
+            }
+
+            if (_storage[n].OutputSignals > elementOutputSignals)
+                return n - 1;
+            else
+                return n + 1;
+        }
+    }
+}
